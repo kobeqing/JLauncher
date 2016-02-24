bcf9fa2c24cf574115e8fc044078f8d285a1b900
ejiaqsu <jiaqing.sun@ericsson.com>
2016-02-24 09:09:28 +0800

    the first version of JLauncher

---
+++ app/src/main/java/com/j/kq/jlauncher/MainActivity.java
@@ -0,0 +1,79 @@
+package com.j.kq.jlauncher;
+
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.List;
+
+import com.j.kq.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    private List<ResolveInfo> apps;
+    private void loadApps() {
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        new ImageView(MainActivity.this);
+
+        apps = getPackageManager().queryIntentActivities(mainIntent, 0);
+    }
+
+
+    public class AppsAdapter extends BaseAdapter {
+
+        public AppsAdapter(){
+        }
+
+        @Override
+        public int getCount() {
+            return apps.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return apps.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView iv;
+
+            if(view == null){
+                iv = new ImageView(MainActivity.this);
+                iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                iv.setLayoutParams(new GridView.LayoutParams(50, 50));
+            } else {
+                iv = (ImageView) view;
+            }
+            ResolveInfo info = apps.get(i);
+            iv.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
+            return iv;
+        }
+    }
+}
